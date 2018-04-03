7bfdc20d4dc4a5ffde51ff4c5a675f6d4925a21b
vncode247 <vncode247@gmail.com>
2018-04-03 17:37:21 +0700

    first commit

---
+++ app/src/main/java/com/vncode247/splashscreen/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.vncode247.splashscreen
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // create splash screen activity
+    }
+}
